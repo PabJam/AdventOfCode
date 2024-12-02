68ac590821f797b80a7d816647d9da218f6ee2a7
PabJam <jamin.pablo@googlemail.com>
2024-12-02 13:25:51 +0100

    Added Project

---
+++ 2024/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _2024
+{
+    using Utils;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Day02.Part_2(Day02.input_2).ToString());
+        }
+    }
+}
