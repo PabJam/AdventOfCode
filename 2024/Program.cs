b46a1ff8b7a228e74486eea601d324a90ceaa6c1
PabJam <jamin.pablo@googlemail.com>
2024-12-07 15:36:19 +0100

    Day07

--- 2024/Program.cs
+++ 2024/Program.cs
@@ -5,9 +5,7 @@
     {
         static void Main(string[] args)
         {
-            
-            //Console.WriteLine(Day06.Part_2(Day06.test_input_2).ToString());     
-            Console.WriteLine(Day06.Part_2(Day06.input_2).ToString());     
+            Console.WriteLine(Day07.Part_2(Day07.input_2).ToString());     
         }
     }
 }
--- Utils/DataUtils.cs
+++ Utils/DataUtils.cs
@@ -6,8 +6,29 @@ using System.Threading.Tasks;
 
 namespace Utils
 {
-    static class DataUtils
+    public static class DataUtils
     {
+        /// <summary>
+        /// Turns an int into a string with a base of the characters specified in baseChars
+        /// </summary>
+        public static string IntToString(int value, char[] baseChars)
+        {
+            // 32 is the worst cast buffer size for base 2 and int.MaxValue
+            int i = 32;
+            char[] buffer = new char[i];
+            int targetBase = baseChars.Length;
 
+            do
+            {
+                buffer[--i] = baseChars[value % targetBase];
+                value = value / targetBase;
+            }
+            while (value > 0);
+
+            char[] result = new char[32 - i];
+            Array.Copy(buffer, i, result, 0, 32 - i);
+
+            return new string(result);
+        }
     }
 }
