8b61acb993f60ccd50e80c7bead564c8983c828d
PabJam <jamin.pablo@googlemail.com>
2024-12-07 15:47:38 +0100

    added Timer

--- 2024/Program.cs
+++ 2024/Program.cs
@@ -1,11 +1,16 @@
 ﻿namespace _2024
 {
+    using System.Diagnostics;
     using Utils;
     internal class Program
     {
         static void Main(string[] args)
         {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
             Console.WriteLine(Day07.Part_2(Day07.input_2).ToString());     
+            timer.Stop();
+            Console.WriteLine(timer.Elapsed.ToString());
         }
     }
 }
